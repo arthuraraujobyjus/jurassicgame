d3c1a8c5e12a4c9927838e14c8848c4447ba130e
arthuraraujobyjus <100610370+arthuraraujobyjus@users.noreply.github.com>
2022-03-14 21:08:10 -0300

    projeto11

--- sketch.js
+++ sketch.js
@@ -1,47 +1,54 @@
-var sea,ship;
-var seaImg,shipImg;
+var path,boy, leftBoundary,rightBoundary;
+var pathImg,boyImg;
+var i;
+
 
 function preload(){
-  // Descomente o código para adicionar animação ao navio
-
-  shipImg1 = loadAnimation("ship-1.png");
-  //shipImg1 = loadAnimation("ship-1.png");
-  //shipImg1 = loadAnimation("ship-1");
-  //shipImg1 = loadAnimation("ship-1.png","ship-2.png","ship-1.png","ship-2.png");
-  //shipImg1 = loadAnimation("ship-1","ship-2","ship-1","ship-2");
-  
-  seaImg = loadImage("sea.png");
+pathImg = loadImage("path.png");
+boyImg = loadAnimation("Runner-1.png","Runner-2.png");
 }
 
 function setup(){
-  createCanvas(400,400);
-  background("blue");
+ createCanvas(400,400);
 
-  // Movendo o fundo
-  sea=createSprite(400,200);
-  sea.addImage(seaImg);
-  sea.velocityX = -5;
-  sea.scale=0.3;
+// Movendo o fundo 
+ path=createSprite(200,200);
+ path=addImage(pathImg);
+ path=velocityY=4;
+ path=scale=1.2;
 
-  
-  ship = createSprite(130,200,30,30);
-  ship.addAnimation("movingShip",shipImg1);
-  ship.scale =0.25;
-  
+//Criando menino que corre 
+
+
+
+// Criando Boundary (Limite) esquerdo  
+leftBoundary=createSprite(0,0,100,800);
+leftBoundary.visible = false;
+
+
+//Crie Boundary direito 
+
 }
+
 
 function draw() {
-  background(0);
-  sea.velocityX = -3;
+background(0);
+path.velocityY = 4;
 
-  // Descomente o código para redefinir o fundo
-  if(sea.x < 0){
-    //sea.x = 0;
-    //sea.x = sea.width;
-    //sea.x = sea.width/8;
-    //sea.y = height;
-  }
+// Menino se movendo no eixo X com o mouse
+boy.x=world.mouseX;
 
- 
-  drawSprites();
-}
+edges= createEdgeSprites();
+boy.collide(edges[3]);
+boy.collide(leftBoundary);
+boy.collide(rightBoundary);
+
+ //Reiniciar o fundo
+
+ drawSprites();
+
+ }
+
+
+
+
